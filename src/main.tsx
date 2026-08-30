78ccec11f10af006a6b589ae91fd82ff1b88bb3e
foli4ier <40297634+foli4ier@users.noreply.github.com>
2026-08-29 21:28:08 -0700

    feat: import daup-theme and owner chrome CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'daup-theme/fonts.css';
+import 'daup-theme/tokens.css';
 import './index.css';
+import './owner.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
